38f9e37f05424ed5ee25c465dcfd25f0fd48eb56
unknown <kanijtasrin090@gmail.com>
2021-12-09 01:19:18 +0600

    final deploy

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,6 @@ export default function Home() {
             <HeroSection />
             <PortfolioSection />
             <CVSection />
-            <BlogSection />
             <LastSection />
           </div>
           <div className="socialSideBar">
